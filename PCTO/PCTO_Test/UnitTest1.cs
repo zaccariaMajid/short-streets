610ae0316abe56ef4a530525704316f2496e2032
BissolaMattia <62284595+BissolaMattia@users.noreply.github.com>
2021-02-17 18:30:24 +0100

    Prove

--- PCTO/PCTO_Test/UnitTest1.cs
+++ PCTO/PCTO_Test/UnitTest1.cs
@@ -1,14 +1,28 @@
 using System;
 using Xunit;
+using System.Collections.Generic;
+using FluentAssertions;
 
 namespace PCTO_Test
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestPositiveNumber()
         {
+            //arrange
+            List<int> List = new List<int>();
+            
 
+
+
+
+            //act
+
+
+
+
+            //assert
         }
     }
 }
