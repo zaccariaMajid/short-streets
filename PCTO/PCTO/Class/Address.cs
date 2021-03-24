ba0c0dca58a5bcfe19f0d522b3237bebf9867de0
Dan <giuggiolidaniel@gmail.com>
2021-03-24 18:14:01 +0100

    api latitudine longitudine

--- PCTO/PCTO/Class/Address.cs
+++ PCTO/PCTO/Class/Address.cs
@@ -10,9 +10,9 @@ using System.IO;
 
 namespace PCTO
 {
-    class Address
+    public class Address
     {
-        public Address(string number = "29", string road = "Via Mauro Gavazzeni", string town = "Bergamo", string province = "BG")
+        public Address(string number = "29a", string road = "Via Gavazzeni", string town = "Bergamo", string province = "BG")
         {
             this._number = number;
             this._road = road;
@@ -22,6 +22,10 @@ namespace PCTO
         public override string ToString()
         {
             return $"{this.Road} {this.Number}, {this.Town} ({this.Province})";
+        }
+        public string ToCompleteAddress()
+        {
+            return $"{this.Number} {this.Road}, {this.Town}";
         }
 
         string _number;
@@ -50,22 +54,73 @@ namespace PCTO
         {
             get { return _province; }
             set { _province = PropertyControl.Province(value); }
+        }      
+
+        
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+
+    public interface IApiCaller
+    {
+        ResultApi GetApiResult(String completeAddress);
+    }
+
+    public class ApiCaller : IApiCaller
+    {
+        public ResultApi GetApiResult(string completeAddress)
+        {
+            var apiUrl = $"http://api.positionstack.com/v1/forward?access_key=1555307b64886ccfe7546a7037046834&query={completeAddress}&output=json";
+            string jsonString = string.Empty;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(apiUrl);
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+                jsonString = reader.ReadToEnd();
+
+             return JsonConvert.DeserializeObject<ResultApi>(jsonString);
+        }
+    }
+
+    public class CoordinateHelper
+    {
+        private readonly IApiCaller caller;
+
+        public CoordinateHelper(IApiCaller caller)
+        {
+            this.caller = caller;
+        }        
+
+        public List<Coordinates> GetCoordinates(string completeAddress)
+        {            
+            completeAddress = WebUtility.UrlEncode(completeAddress);
+            var stuff = caller.GetApiResult(completeAddress);            
+            return stuff.Data.Select(x => new Coordinates() { Latitude = x.Latitude, Longitude = x.Longitude }).ToList();
         }
 
-        //void a()
-        //{
-        //    string jsonString = string.Empty;
-        //    HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://positionstack.com/");
-        //    using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-        //    using (Stream stream = response.GetResponseStream())
-        //    using (StreamReader reader = new StreamReader(stream))
-        //        jsonString = reader.ReadToEnd();
-        //    dynamic stuff = JsonConvert.DeserializeObject(jsonString);
+        public void SetCoordinates( Address address)
+        {
+            var a = GetCoordinates(address.ToCompleteAddress());
+            address.Latitude = a[0].Latitude;
+            address.Longitude = a[0].Longitude;
+        }
+    }
 
-        //}
+    public class Coordinates
+    {
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
 
-
-        public string Latitude { get; set; }
-        public string Longitude { get; set; }
+    public class ResultApi
+    {
+        public ApiData[] Data { get; set; }
+        public class ApiData
+        {
+            public decimal Latitude { get; set; }
+            public decimal Longitude { get; set; }
+            public string Postal_code { get; set; }
+            public string Map_url { get; set; }
+        }
     }
 }
