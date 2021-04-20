82d26878e2193037c700d23f179da700b65aa4a2
zaccariaMajid <zaccariam45@gmail.com>
2021-04-20 18:00:35 +0200

    Commit test

--- PCTO/PCTO_Test/UnitTest1.cs
+++ PCTO/PCTO_Test/UnitTest1.cs
@@ -3,7 +3,7 @@ using Xunit;
 using System.Collections.Generic;
 using FluentAssertions;
 using PCTO;
-using PCTO.Address;
+//using PCTO.Address;
 using Moq;
 using System.Text;
 using static PCTO.ApiResult;
@@ -14,7 +14,7 @@ namespace PCTO_Test
 {
     public class UnitTest1
     {
-       
+        #region Coordinates
         [Fact]
         public void SetCordinates_ShouldWork()
         {
@@ -61,8 +61,11 @@ namespace PCTO_Test
             a.Coordinates.Lat.Should().BeInRange(39.2M,39.3M);
             a.Coordinates.Lng.Should().BeInRange(9.1M, 9.2M);          
         }
+        #endregion
+
+        #region Province
         [Fact]
-        public void Controlprovince1()
+        public void ControlprovinceUPPER()
         {
             //Arrange
             string province = "BG";
@@ -73,46 +76,231 @@ namespace PCTO_Test
 
         }
         [Fact]
-        public void Controlprovince2()
+        public void ControlprovinceLOWER1()
         {
+            //Arrange
             var provincia = "mi";
-            var result = Province(provincia);
-            result.Should().Be(null);
-            Assert.Throws<ArgumentException>(() => result);
+            string result=default(string);
+            try
+            {   //Act             
+                result = Province(provincia);                
+            }
+            catch
+            {
+                //Assert
+                result.Should().NotBe(provincia);
+            }
         }
+        [Fact]
+        public void ControlprovincefirstUPPER1_2()
+        {
+            //Arrange
+            var provincia = "Mi";
+            string result = default(string);
+            try
+            {   //Act             
+                result = Province(provincia);
+            }
+            catch
+            {
+                //Assert
+                result.Should().NotBe(provincia);
+            }
+        }
+        [Fact]
+        public void ControlprovincefirstLOWER1_3()
+        {
+            //Arrange
+            var provincia = "mI";
+            string result = default(string);
+            try
+            {   //Act             
+                result = Province(provincia);
+            }
+            catch
+            {
+                //Assert
+                result.Should().NotBe(provincia);
+            }
+        }
+        [Fact]
+        public void ControlprovinceLOWER2_1()
+        {
+            //Arrange
+            var provincia = "mi";
+            var ex = false;
+            try
+            {
+                //Act
+                Province(provincia);
+            }
+            catch
+            {
+                ex = true;
+            }
+            //Assert
+            ex.Should().Be(true);
+        }
+
+        [Fact]
+        public void ControlprovincefirstUPPER2_2()
+        {
+            //Arrange
+            var provincia = "Mi";
+            var ex = false;
+            try
+            {
+                //Act
+                Province(provincia);
+            }
+            catch
+            {
+                ex = true;
+            }
+            //Assert
+            ex.Should().Be(true);
+        }
+        [Fact]
+        public void ControlprovincefirstLOWER2_3()
+        {
+            //Arrange
+            var provincia = "mI";
+            var ex = false;
+            try
+            {
+                //Act
+                Province(provincia);
+            }
+            catch
+            {
+                ex = true;
+            }
+            //Assert
+            ex.Should().Be(true);
+
+        }
+        [Fact]
+        public void controlexfirstLOWER()
+        {   //Arrange
+            var nome = new Address("29a", "Via Gavazzeni", "Bergamo", "bG");
+            var g = nome.Province;
+            //Assert
+            Assert.Throws<ArgumentException>(() => Province(g));
+        }
+        [Fact]
+        public void controlexLOWER()
+        {
+            //Arrange
+            var nome = new Address("29a", "Via Gavazzeni", "Bergamo", "bg");
+            var g = nome.Province;
+            //Assert
+            Assert.Throws<ArgumentException>(() => Province(g));
+        }
+        [Fact]
+        public void controlexfirstUPPER()
+        {
+            //Arrange
+            var nome = new Address("29a", "Via Gavazzeni", "Bergamo", "Bg");
+            var g = nome.Province;
+            //Assert
+            Assert.Throws<ArgumentException>(() => Province(g));
+        }
+        [Fact]
+        public void controlequalexfirstLOWER()
+        {
+            //Assert
+            var nome = new Address("29a", "Via Gavazzeni", "Bergamo", "bG");
+            var g = nome.Province;
+            //Act
+            var ex = Assert.Throws<ArgumentException>(() => Province(g));
+            //Assert
+            Assert.Equal("Province must be a 2 upper chars string", ex.Message);
+        }
+        [Fact]
+        public void controlequalexLOWER()
+        {
+            //Assert
+            var nome = new Address("29a", "Via Gavazzeni", "Bergamo", "bg");
+            var g = nome.Province;
+            //Act
+            var ex = Assert.Throws<ArgumentException>(() => Province(g));
+            //Assert
+            Assert.Equal("Province must be a 2 upper chars string", ex.Message);
+        }
+        [Fact]
+        public void controlequalexfirstUPPER()
+        {
+            //Assert
+            var nome = new Address("29a", "Via Gavazzeni", "Bergamo", "Bg");
+            var g = nome.Province;
+            //Act
+            var ex = Assert.Throws<ArgumentException>(() => Province(g));
+            //Assert
+            Assert.Equal("Province must be a 2 upper chars string", ex.Message); ;
+        }
+        #endregion 
+
+        #region Number
         [Fact]
         public void NegativeNumber1()
         {
             int Number = 5;
-            PositiveNumber(Number);
+            var result = PositiveNumber(Number);
+            result.Should().Be(Number);
         }
         [Fact]
         public void NegativeNumber2()
         {
-            int Number = -5;
-            PositiveNumber(Number);
+            int Number = 5;
+            int result = default(int);
+            try
+            {
+                result = PositiveNumber(Number);
+            }
+            catch
+            {
+                result.Should().NotBe(Number);
+            }
+            
         }
+        #endregion
+
+        #region String
         [Fact]
         public void ValidString1()
         {
             string word1 = "Abc";
-            ValidString(word1);
+            var result = ValidString(word1);
+            result.Should().Be(word1);
         }
         [Fact]
         public void ValidString2()
         {
-            string word2 = "";
-            ValidString(word2);
+            //Arrange
+            string word = null;
+            string result = default(string);
+            try
+            {   //Act             
+                result = ValidString(word);
+            }
+            catch
+            {
+                //Assert
+                result.Should().Be(null);
+            }
         }
-
         [Fact]
         public void ControlAddress()
         {
             var Address = new Address("30a", "Via Manzoni", "Milano", "MI");
-            Address.ToCompleteAddress();
+            var result = Address.ToCompleteAddress();
+            result.Should().Be("Via Manzoni 30a Milano");
+            result.Length.Should().Be(22);
         }
-        
-       [Fact]
+        #endregion
+
+        #region API
+        [Fact]
        public void FirstControlCoordinates()
         {
             Address a = new Address("32", "Via Giacomo Manzù", "Terno d'Isola", "BG");
@@ -147,7 +335,7 @@ namespace PCTO_Test
             a.Coordinates.Lat.Should().BeInRange(45.7074M, 45.7105M);
             a.Coordinates.Lng.Should().BeInRange(9.6787M, 9.6826M);
         }
-        
-        
+        #endregion
+
     }
 }
