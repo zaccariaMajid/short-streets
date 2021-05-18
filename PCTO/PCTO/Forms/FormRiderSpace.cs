8a5e6dd4672577e3abbce1bb746f80c5800445ea
nicosonzo03 <nicolo.sonzogni03@gmail.com>
2021-05-18 14:03:04 +0200

    Revert "Aggiornamento"
    This reverts commit 0a95d71e6ad93222d677b971e035816bc307c743.

--- PCTO/PCTO/Forms/FormRiderSpace.cs
+++ PCTO/PCTO/Forms/FormRiderSpace.cs
@@ -65,7 +65,7 @@ namespace PCTO
                 helper.SetCoordinates(currentPackage.Destination);
                 if(!CoordinatesRangeManager.IsInRange(fShortStreets.coordinatesRange, currentPackage.Destination.Coordinates))
                 {
-                    MessageBox.Show($"Packages coordinates are out of range ({fShortStreets.coordinatesRange})");
+                    MessageBox.Show($"Packages' coordinates are out of range ({fShortStreets.coordinatesRange})");
                     return;
                 }
                 packages[packages.IndexOf(packages
@@ -81,7 +81,7 @@ namespace PCTO
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message);
+                MessageBox.Show($"Generic error: {ex.Message}");
                 return;
             }
         }
@@ -91,38 +91,9 @@ namespace PCTO
             fShortStreets.packages.Clear();
             foreach (var p in packages)
                 fShortStreets.packages.Add(p.ToPackage());
-            if (fShortStreets.currentAddress == null)
-            {
-                MessageBox.Show("Set your current position to access to the map (home)");
-                return;
-            }
-            if (fShortStreets.packages.Where(x => x.IsValid == false).ToList().Count != 0)
-            {
-                MessageBox.Show("Convalid all packages to continue");
-                return;
-            }
             fShortStreets.ShowFormMap();
         }
 
-        private void dgvSetPackages_CellValueChanged(object sender, DataGridViewCellEventArgs e)
-        {
-            //    try
-            //    {
-            //        FormElaboration.ChangePackagePropertyFromDgv(currentPackage, dgvSetPackages);
-            //    }
-            //    catch(ArgumentException argEx)
-            //    {
-            //        MessageBox.Show(argEx.Message);
-            //        return;
-            //    }
-        }
-
-        private void dgvSetPackages_CellEnter(object sender, DataGridViewCellEventArgs e)
-        {
-            //    if (dgvSetPackages.SelectedCells.Count < 1)
-            //        return;
-            //    currentPackage = FormElaboration.GetPackageByDgvCell(dgvSetPackages);
-        }
         void BindEditGpb()
         {
             gpbEdit.Enabled = true;
